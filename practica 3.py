01f406adf1d1213ef885a847c6b82352c4a9c788
Alberto Martinez <alberto1230@gmail.com>
2025-10-11 10:45:51 -0600

    ejercicio 3

---
+++ practica 3.py
@@ -0,0 +1,10 @@
+# Programa 3: Mostrar el mayor de tres números
+
+a = float(input("Ingresa el primer número: "))
+b = float(input("Ingresa el segundo número: "))
+
+c = float(input("Ingresa el tercer número: "))
+
+mayor = max(a, b, c)
+print("El número mayor es:", mayor)
+
