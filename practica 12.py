41677df2278ecc5433b9702f7d590a5a0397ddd6
Alberto Martinez <alberto1230@gmail.com>
2025-10-15 16:48:48 -0600

    ejercicio 12

---
+++ practica 12.py
@@ -0,0 +1,8 @@
+numero = int(input("Ingresa un numero y se imprimira el resto de numeros hasta el ingresado: "))
+
+contador=1
+
+while contador<=numero:
+    print ("El contador va por: ", contador)
+    contador+=1
+    
